453a2be8d37f3ecf07db3b35a3ff5582e95a4a67
Haomiao Jiang <hjiang36@gmail.com>
2013-07-23 00:18:57 -0700

    Update image croping and SVM parameters

--- scripts/s_coneAbsorptionsColorTest.m
+++ scripts/s_coneAbsorptionsColorTest.m
@@ -7,21 +7,22 @@
 % HJ VISTASOFT Team 2013
 
 %% Create two scenes with slightly different colors
-
-% ---- Scene ----------
-% fov         = 2;               % deg. Make sure we have plenty of scene samples per cone sample. By making the FOV small, we get plenty.
-fov         =  0.2;             % shrink the fov to speed up calculations? 
+%  Set Parameters
+fov         =  0.2;             % field of view
 dCal        = 'LCD-Apple.mat';
 vd          = 2;                % Viewing distance- Two meters
 
 wave = [540 550];
 sz = 128;
+
+% Create Scene 1
 scene1 = sceneCreate('uniform monochromatic',wave,sz);
 scene1 = sceneSet(scene1,'fov',fov);       %
 scene1 = sceneSet(scene1,'distance',vd);  % Two meters
 scene1 = sceneSet(scene1,'name','545');
 % vcAddAndSelectObject(scene1); sceneWindow
 
+% Create Scene 2
 wave = [550 560];
 scene2 = sceneCreate('uniform monochromatic',wave,sz);
 scene2 = sceneSet(scene2,'fov',fov);       %
@@ -51,36 +52,36 @@ sensor2 = sensorComputeNoiseFree(sensor,oi2);
 % vcAddAndSelectObject(sensor1); sensorWindow;
 
 
-nSamples = 40;    % Number of trials
+nSamples = 400;    % Number of trials
 noiseType = 1;    % Just photon noise
 voltImages1 = sensorComputeSamples(sensor1,nSamples,noiseType);
 voltImages2 = sensorComputeSamples(sensor2,nSamples,noiseType);
 
 % Found this once using 
 % [locs,rect] = vcROISelect(sensor1)
-rect = [29    22    30    29];
+rect = [29    22    5    5];
 
-% Use imcrop to select out the voltImages1/2 with that rect
+% Crop Images by rect
+voltImages1 = voltImages1(rect(2):rect(2)+rect(4),rect(1):rect(1)+rect(3),:);
+voltImages2 = voltImages2(rect(2):rect(2)+rect(4),rect(1):rect(1)+rect(3),:);
 
 %% Training
-
-% Let's give better names to I and Y and so forth
 ind = randperm(2*nSamples);
 [row,col,~] = size(voltImages1);
-I_c         = reshape(permute(voltImages1,[3 1 2]),[nSamples, row*col]);
+dataMatrix1 = reshape(permute(voltImages1,[3 1 2]),[nSamples, row*col]);
 [row,col,~] = size(voltImages2);
-I_n         = reshape(permute(voltImages2,[3 1 2]),[nSamples, row*col]);
-I_train = [I_c; I_n];
-Y = [ones(nSamples,1);-ones(nSamples,1)];
+dataMatrix2 = reshape(permute(voltImages2,[3 1 2]),[nSamples, row*col]);
+I_train = [dataMatrix1; dataMatrix2];
+groupLabels = [ones(nSamples,1);-ones(nSamples,1)];
 
 % Train and SVM structure explain flags here
 svmStruct = ...
-    svmtrain(Y(ind(1:round(1.8*nSamples))),...
-    sparse(I_train(ind(1:round(1.8*nSamples)),:)),'-t 1 -q');
+    svmtrain(groupLabels(ind(1:round(1.8*nSamples))),...
+    sparse(I_train(ind(1:round(1.8*nSamples)),:)),'-t 1');
 
 % Predictions and accuracy
 [predLabels,curAcc,~] = ...
-     svmpredict(Y(ind(round(1.8*nSamples)+1:end)),...
+     svmpredict(groupLabels(ind(round(1.8*nSamples)+1:end)),...
      sparse(I_train(ind(round(1.8*nSamples)+1:end),:)),...
      svmStruct,'-q');
  
